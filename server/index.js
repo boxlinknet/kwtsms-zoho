c8e812625dc58bd66db0d1767a204515354ac4c2
kwtSMS Admin <support@kwtsms.com>
2026-03-18 22:03:11 +0300

    Fix ZET dev server: proper HTTPS server, SSL certs, full dependencies
    - Rewrite server/index.js as self-executing HTTPS Express server
      (ZET requires this pattern, not a module.exports function)
    - Add all ZET-required dependencies (body-parser, errorhandler, morgan,
      serve-index, chalk, i18next)
    - Generate self-signed SSL cert/key for local HTTPS on port 5000
    - Add cert.pem/key.pem to .gitignore (generated locally)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,50 @@
-var express = require('express');
+var fs = require('fs');
 var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var morgan = require('morgan');
+var serveIndex = require('serve-index');
+var https = require('https');
+var chalk = require('chalk');
 
-module.exports = function(app) {
-    app.use('/app', express.static(path.join(__dirname, '..', 'app')));
+process.env.PWD = process.env.PWD || process.cwd();
+
+var expressApp = express();
+var port = 5000;
+
+expressApp.set('port', port);
+expressApp.use(morgan('dev'));
+expressApp.use(bodyParser.json());
+expressApp.use(bodyParser.urlencoded({ extended: false }));
+expressApp.use(errorHandler());
+
+expressApp.use('/', function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+expressApp.get('/plugin-manifest.json', function(req, res) {
+    res.sendFile(path.join(process.env.PWD, 'plugin-manifest.json'));
+});
+
+expressApp.use('/app', express.static('app'));
+expressApp.use('/app', serveIndex('app'));
+
+expressApp.get('/', function(req, res) {
+    res.redirect('/app');
+});
+
+var options = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
 };
+
+https.createServer(options, expressApp).listen(port, function() {
+    console.log(chalk.green('Zet running at https://127.0.0.1:' + port));
+    console.log(chalk.bold.cyan('Note: Please enable the host (https://127.0.0.1:' + port + ') in a new tab and authorize the connection by clicking Advanced->Proceed to 127.0.0.1 (unsafe).'));
+}).on('error', function(err) {
+    if (err.code === 'EADDRINUSE') {
+        console.log(chalk.bold.red(port + ' port is already in use'));
+    }
+});
